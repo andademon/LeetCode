589f08eb6de7ff2051eb6916f8d35b8b3e9b0642
HAL101010 <3218497724@qq.com>
2023-03-20 18:31:09 +0800

    2023/3/20

--- javascript/32_最长有效括号/main.js
+++ javascript/32_最长有效括号/main.js
@@ -3,24 +3,50 @@
  * @return {number}
  */
 var longestValidParentheses = function(s) {
-    let temp,max_len = 0;
-    let stack1 = [...s];
-    let stack2 = [];
-    let stack3 = [];
-    while(stack1.length !== 0){
-        temp = stack1.pop();
-        if(temp === '('){
-            if(stack2.pop() === ')'){
-                stack3.push('(');
+    let array = [...s];
+    let i = 0,j = 0,max_len = 0,len = 0;
+    let stack = [];
+    let status = new Array(s.length);//记录所有括号匹配状态
+    status.forEach(value => {
+        value = 0;//表示尚未匹配成功
+    })
+    while(array[i] == ')' && i < array.length){
+        i++;
+    }
+    for(;i < array.length;i++){
+        if(array[i] == '('){
+            stack.push(i);
+            // rs += '(';
+        }else{
+            let temp = stack.pop();
+            if(temp != undefined){//匹配成功
+                status[temp] = 1;
+                status[i] = 1;
+            }else{//匹配失败
+                stack = new Array();
                 continue;
             }
-            else{
-
-            }
-        }else{
-            stack2.push(temp);
         }
     }
-    if(stack1.length === 0 && stack2.length === 0) return true;
-    else return false;
+    for(i = 0;i < status.length;i++){
+        if(status[i] == 1){
+            j = i + 1;
+            while(status[j] == 1){
+                j++;
+            }
+            len = j - i;
+            if(len > max_len){
+                max_len = len;
+            }
+        }
+    }
+    return max_len;
 };
+
+// let s = ")()())";
+
+let s = "()(()(()(())()";
+
+let test = [];
+
+console.log(test.pop())
