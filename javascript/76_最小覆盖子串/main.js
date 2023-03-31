289ba24069beb7f43145c40f2a63ae50ced48cee
HAL101010 <3218497724@qq.com>
2023-03-31 15:10:59 +0800

    2023/3/31

--- javascript/76_最小覆盖子串/main.js
+++ javascript/76_最小覆盖子串/main.js
@@ -4,9 +4,11 @@
  * @return {string}
  */
 
+//超时了，在main2改进一下算法
+
 //滑动窗口求解
 
-function getIndex(s,c,temp_array){
+function getIndex(s,c,temp_array){//原字符串，要找的字符，辅助数组(防止重复字符下标相同)
     for(let i = 0;i < s.length;i++){
         if(s[i] === c && temp_array[i] === 1){
             temp_array[i] = 0;
@@ -17,24 +19,45 @@ function getIndex(s,c,temp_array){
 }
 
 var minWindow = function(s, t) {
-    let array = new Array();
-    // for(let i = 0;i < s.length;i++){
-    //     for(let j = 0;j < t.length;j++){
-    //         if(t[j] == s[i]){
-    //             array[j] == 0;
-    //             break;
-    //         }
-    //     }
-    // }
-    let stack = [...t];
-    let temp_array = new Array(s.length).fill(1);
-    while(stack.length){
-        let c = stack.pop();
-        let index = getIndex(s,c,temp_array)
+    let array = new Array();//array数组,存的是找到第一个包含所有字符的子串的各个字母的下标
+    let temp_array = new Array(s.length).fill(1);//辅助数组，表示串s在该位置上的字符是否已被使用
+    for(let i = 0;i < t.length;i++){
+        let index = getIndex(s,t[i],temp_array);
         if(index != -1){
             array.push(index);
         }else{
             return "";//如果s中找不到t所属字符直接return 空串
         }
     }
+    array.sort((a,b) => a-b);
+    // let min = array[array.length - 1] - array[0] + 1;
+    let min = {
+        len:array[array.length - 1] - array[0] + 1,
+        start:array[0],
+        end:array[array.length - 1]
+    }
+    //下面开始滑动窗口
+    for(let i = 0;array[i] < s.length;){
+        let c = s[array[i]];
+        let index = getIndex(s,c,temp_array);
+        if(index != -1){
+            array[0] = index;
+            array.sort((a,b) => a-b);
+            let newlen = array[array.length - 1] - array[0] + 1;
+            if(min.len > newlen){
+                min.len = newlen;
+                min.start = array[0];
+                min.end = array[array.length - 1];
+            }
+            continue;
+        }else{
+            return s.slice(min.start,min.end + 1);
+        }
+    }
 };
+
+let s = "ADOBECODEBANC";
+
+let t = "ABC";
+
+console.log(minWindow(s,t))
---
+++ javascript/76_最小覆盖子串/main2.js
@@ -0,0 +1,81 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+
+//超时了，改进一下算法
+
+//滑动窗口求解
+
+function containAll(hashMap_window,hashMap_t){//hashMap_window是否包含hashMap_t全部元素及其数量
+    for(key in hashMap_t){
+        if(hashMap_window[key] >= hashMap_t[key]) continue;
+        else return false;
+    }
+    return true;
+}
+
+var minWindow = function(s, t) {
+    let hashMap_t = {};//第一个哈希表，表示t中字母及其个数
+    let hashMap_window = {};//第二个哈希表，表示滑动窗口中所有字符及它们个数
+    for(let i = 0;i < t.length;i++){
+        isNaN(hashMap_t[t[i]]) ? hashMap_t[t[i]] = 1 : hashMap_t[t[i]]++ 
+    }
+    let left = 0, right = 0;
+    let min = {
+        len:s.length + 1,//确保一开始时min比所有解都大
+        left:0,
+        right:0
+    }
+    while(right < s.length){
+        while(!containAll(hashMap_window,hashMap_t) && right < s.length){//扩张直到窗口包含可行解
+            isNaN(hashMap_window[s[right]]) ? hashMap_window[s[right]] = 1 : hashMap_window[s[right]]++
+            right++;
+        }
+        if(right == s.length){
+            if(!containAll(hashMap_window,hashMap_t)) break;
+        }
+        while(containAll(hashMap_window,hashMap_t)){//收缩直到可行窗口最小
+            hashMap_window[s[left]]--;
+            left++;
+        }
+        //上次循环结束后不可行,left退回可行位置
+        left--;
+        hashMap_window[s[left]]++;
+        //以下是对一种可行解的处理
+        if(right == s.length){
+            right--;
+            if(min.len > right - left){
+                min.len = right - left + 1;
+                min.left = left;
+                min.right = right;
+            }
+            break;
+        }
+        if(min.len > right - left){
+            min.len = right - left;
+            min.left = left;
+            min.right = right - 1;
+            left++;
+            right = left;
+            hashMap_window = {};
+            continue;
+        }else{
+            left++;
+            right = left;
+            hashMap_window = {};
+        }
+    }
+    if(min.len > s.length) return "";
+    else return s.slice(min.left,min.right + 1);
+};
+
+let s = "ADOBECODEBANC";
+// let s = "a"
+
+let t = "ABC";
+// let t = "a"
+
+console.log(minWindow(s,t))
+// minWindow(s,t)
