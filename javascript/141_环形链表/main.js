a8faceb3c8cf2a0d7a8cb6298742b2385e724e4a
HAL101010 <3218497724@qq.com>
2023-04-11 15:23:21 +0800

    2023/4/11

---
+++ javascript/141_环形链表/main.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+class ListNode{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+//额外空间
+// var hasCycle = function(head) {
+//     let node = head;
+//     while(node){
+//         if(node.arrived == undefined){
+//             node.arrived = true;
+//             node = node.next;
+//         }else return true;
+//     }
+//     return false;
+// };
+
+//快慢指针
+var hasCycle = function(head) {
+    if(head == null || head.next == null) return false;
+    let slow = head;
+    let fast = head.next;
+    while(fast != slow){
+        if(fast == null || fast.next == null) return false;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return true;
+}
